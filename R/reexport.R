0bf97fcea8085b9032e25b0b50ae046873fe9384
Ben Moore <b3nmoore@gmail.com>
2017-05-20 23:38:25 +0100

    docs update

--- R/reexport.R
+++ R/reexport.R
@@ -1,3 +1,9 @@
+#' Re-exported read_json function
+#'
+#' This function is imported from the
+#' \code{jsonlite} package, see its documentation
+#' there: \code{\link[jsonlite]{read_json}}.
+#'
 #' @importFrom jsonlite read_json
 #' @export
-jsonlite::read_json
+read_json <- jsonlite::read_json
