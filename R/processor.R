23e1a64fae15a91bc1b5c00080ea22a383214712
Ben Moore <b3nmoore@gmail.com>
2017-05-20 20:17:35 +0100

    simple index, subset

--- R/processor.R
+++ R/processor.R
@@ -8,19 +8,14 @@ test_num <- function(charnum) {
 }
 
 #' Extract sub-list from list
-get_listitem <- function(json_list, part, zero_index) {
+get_listitem <- function(json, part, zero_index) {
 
-  subparts <- stringr::str_split_fixed(
-    substr(part, 2, nchar(part)), "\\]", 2)
-  index <- subparts[[1]]
-  field <- subparts[[2]]
-
+  index <- gsub("\\[|\\]", "", part)
   # range subset
   if (grepl(":", index)) {
     indices <- stringr::str_split_fixed(":", index, 2)
-    range <- as.numeric(stringr::str_split_fixed(field_id, ":", 2))
+    range <- as.numeric(stringr::str_split_fixed(index, ":", 2))
     range <- if (zero_index) range + 1 else range
-    print(range)
     return(`[`(json, seq(range[[1]], range[[2]])))
   } else {
     # * or index
@@ -29,6 +24,10 @@ get_listitem <- function(json_list, part, zero_index) {
     } else {
       index <- test_num(index)
       index <- if (zero_index) index + 1 else index
+      if (index > length(json)) {
+        stop("subscript ", part, " longer than array ",
+          "length (", length(json), ")")
+      }
       this_json <- json[[index]]
     }
   }
@@ -45,8 +44,8 @@ get_obj <- function(json, name) {
   }
 
   if (!name %in% names(json)) {
-    stop(name, " not found in json (",
-      paste(names(json), collapse = ","))
+    stop(name, " not found in object (names are: ",
+      paste(names(json), collapse = ","), ")")
   }
 
   `[[`(json, name)
@@ -59,7 +58,7 @@ parse_part <- function(part, json, zero_index) {
 
   if (grepl("^\\[", part)) {
     # indexing a list
-    get_fn <- get_listitem(json, part, zero_index = zero_index)
+    this_json <- get_listitem(json, part, zero_index = zero_index)
   } else {
     # requesting a single named object
     this_json <- get_obj(json, part)
@@ -73,17 +72,12 @@ parse_jpath <- function(json, path, zero_index = TRUE) {
   # if not strict, don't skip first
   parts <- stringr::str_split(path, "\\.|\\[", simplify = TRUE)[-1]
   parts <- gsub("^(.*?)\\]", "[\\1]", parts)
-  print(parts)
 
-  this_json <- json
   for (p in parts) {
-    message(p)
-    next_json <- parse_part(p, this_json, zero_index)
-    this_json <- next_json
-    # print(next_json)
+    json <- parse_part(p, json, zero_index)
   }
 
-  this_json
+  json
 }
 
 #' Apply JSONPath to a json object
@@ -120,5 +114,7 @@ json_path <- function(json, path,
 }
 
 
-json <- read_json("tests/testthat/bookstore.json")
-books <- json_path(json, "$.store.book[1].author")
+# json <- read_json("tests/testthat/bookstore.json")
+# json_path(json, "$.store.book[1:2]")
+#
+# json
